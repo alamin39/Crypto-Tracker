e1c796e4f705798f4eb746426a0e3f753a35e74b
Al-Amin <alamin61a@gmail.com>
2023-03-08 18:45:17 +0900

    add a custom navigation link for lazy loading

---
+++ Crypto/Crypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  Crypto
+//
+//  Created by Al-Amin on 2023/03/08.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing detail view for \(coin.name)")
+    }
+    
+    var body: some View {
+        Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
